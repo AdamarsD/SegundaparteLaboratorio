9a2e5fbabf2cd818ec94d6232095393bfad37742
AdamarsD <ad0094032021@unab.edu.sv>
2021-08-30 16:45:10 -0600

    entidades

---
+++ Persona/Persona/entidades/ClsPersona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona.entidades
+{
+    class ClsPersona
+    {
+
+        private String nombre;
+        private String sexo;
+        private int edad;
+        private int peso;
+        private int altura;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Peso { get => peso; set => peso = value; }
+        public int Altura { get => altura; set => altura = value; }
+
+    }
+}
