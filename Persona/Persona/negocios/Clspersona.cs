17f5bf6cdf824717ffa439753ff6fcbaa15898c8
AdamarsD <ad0094032021@unab.edu.sv>
2021-08-31 00:01:44 -0600

    Create Clspersona.cs

---
+++ Persona/Persona/negocios/Clspersona.cs
@@ -0,0 +1,9 @@
+﻿namespace Persona.negocios
+{
+    internal class Clspersona
+    {
+        public double Peso { get; internal set; }
+        public double Altura { get; internal set; }
+        public int Edad { get; internal set; }
+    }
+}
