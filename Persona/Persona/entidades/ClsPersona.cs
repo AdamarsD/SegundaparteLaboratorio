bf62ac3e447658612e7f6c216a868acbb5b08629
AdamarsD <ad0094032021@unab.edu.sv>
2021-08-30 21:24:45 -0600

    Update ClsPersona.cs

--- Persona/Persona/entidades/ClsPersona.cs
+++ Persona/Persona/entidades/ClsPersona.cs
@@ -8,20 +8,5 @@ namespace Persona.entidades
 {
     class ClsPersona
     {
-
-
-        private String nombre;
-        private String sexo;
-        private int edad;
-        private int peso;
-        private int altura;
-
-        public string Nombre { get => nombre; set => nombre = value; }
-        public string Sexo { get => sexo; set => sexo = value; }
-        public int Edad { get => edad; set => edad = value; }
-        public int Peso { get => peso; set => peso = value; }
-        public int Altura { get => altura; set => altura = value; }
-    }
-
     }
 }
