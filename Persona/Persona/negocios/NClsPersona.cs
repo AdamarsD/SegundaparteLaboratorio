b4677de11c345363bc258d92c72620ecc70b2244
AdamarsD <ad0094032021@unab.edu.sv>
2021-08-31 00:01:32 -0600

    Update NClsPersona.cs

--- Persona/Persona/negocios/NClsPersona.cs
+++ Persona/Persona/negocios/NClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Persona.entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,32 @@ namespace Persona.negocios
 {
     class NClsPersona
     {
+
+        Clspersona clspersona = new clspersona(0, 0);
+        public String calcularIMC()
+        {
+
+            double pro = clspersona.Peso / Math.Pow(clspersona.Altura, 2);
+
+            if (pro < 20)
+            {
+                return "Su peso es ideal";
+            }
+            if (pro > 25)
+            {
+                return "Tiene sobre peso";
+            }
+            return "";
+        }
+        public String esMayorDeEdad()
+        {
+            if (clspersona.Edad >= 18)
+            {
+                return "Es Mayor de edad";
+            }
+            else
+                return "No es mayor de edad";
+        }
     }
 }
+
