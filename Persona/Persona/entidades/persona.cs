815f637198f3c853a28b15013fb80fbac113b3b4
AdamarsD <ad0094032021@unab.edu.sv>
2021-08-30 21:24:34 -0600

    Create persona.cs

---
+++ Persona/Persona/entidades/persona.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona.entidades
+{
+    class persona
+    {
+
+        private string nombre;
+        private int edad;
+        private string sexo;
+        private double peso;
+        private double altura;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public double Peso { get => peso; set => peso = value; }
+        public double Altura { get => altura; set => altura = value; }
+
+        public persona(string nombre, int edad, string sexo, double peso, double altura)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public persona(string nombre, int edad, string sexo)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = sexo;
+        }
+
+        public persona(int edad, double peso, double altura)
+        {
+            this.edad = edad;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public persona(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+    }
+}
