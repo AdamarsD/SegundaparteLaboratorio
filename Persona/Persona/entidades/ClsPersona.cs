e526eab9366363c08d877e3565dfeab944812297
AdamarsD <ad0094032021@unab.edu.sv>
2021-08-30 23:16:05 -0600

    Update ClsPersona.cs

--- Persona/Persona/entidades/ClsPersona.cs
+++ Persona/Persona/entidades/ClsPersona.cs
@@ -8,5 +8,31 @@ namespace Persona.entidades
 {
     class ClsPersona
     {
-    }
+
+
+        persona per = new persona(0, 0);
+        public String calcularIMC()
+        {
+
+            double pro = per.Peso / Math.Pow(per.Altura, 2);
+
+            if (pro < 20)
+            {
+                return "Su peso es ideal";
+            }
+            if (pro > 25)
+            {
+                return "Tiene sobre peso";
+            }
+            return "";
+        }
+        public String esMayorDeEdad()
+        {
+            if (per.Edad >= 18)
+            {
+                return "Es Mayor de edad";
+            }
+            else
+                return "No es mayor de edad";
+        }
 }
